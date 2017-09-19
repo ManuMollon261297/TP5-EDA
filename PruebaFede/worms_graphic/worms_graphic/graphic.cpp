6592d19533a97b3840f69063ca18cc7bc9af5a21
federicogodoy <federicogodoy@icloud.com>
2017-09-18 23:53:51 -0300

    Add files via upload

---
+++ PruebaFede/worms_graphic/worms_graphic/graphic.cpp
@@ -0,0 +1,89 @@
+#include "graphic.h"
+
+enum { NOMOTION, WARMUP, WALKING };
+const unsigned int cant_images = 15;
+const unsigned int wwalkingseq_arr[14] = { 4,5,6,7,8,9,10,11,11,12,13,14,15,4 };
+graphic::graphic()
+{
+	
+}
+
+void graphic::create_images_arr() {
+	images_lib = new ALLEGRO_BITMAP*[cant_images];
+}
+
+void graphic::init() {
+	prefix = "Worms Images/wwalking/wwalk-F";
+	walking_count = 0;
+	no_motion_count = 0;
+	warmup_count = 0;
+	state = 0;
+	for (unsigned int i = 0; i < cant_images; i++) {
+		images_lib[i] = nullptr;
+
+	}
+}
+
+graphic::~graphic()
+{
+	for (int i = 0; i < cant_images; i++) {
+		al_destroy_bitmap(images_lib[i]);
+	}
+
+	delete[] images_lib;
+}
+
+
+
+void graphic::load_images() {
+
+	for (unsigned int i = 0; i < cant_images; i++) { // el 1 es por F1, F2.. etc
+		string extension = ".png";
+		string aux = prefix+ to_string(i+1) + extension;
+		printf("%s\n", &aux[0]);
+		images_lib[i] = al_load_bitmap(&aux[0]);
+
+	}
+}
+
+
+
+void graphic::do_walking_step() {
+	switch (state) {
+	case NOMOTION:
+		if (no_motion_count < 5) {
+			printf("nomoves\n");
+			no_motion_count++;
+		}
+		else {
+			no_motion_count = 0;
+			state = WARMUP; // Si termine el no motion state voy a warmup
+		}
+
+		break;
+	case WARMUP:
+		if (warmup_count < 3) {
+			al_draw_bitmap(images_lib[warmup_count], 100.0, 100.0, 0);
+			printf("K=%d",warmup_count);
+			warmup_count++;
+		}
+		else {
+			state = WALKING;
+			warmup_count = 0;
+		}
+		break;
+
+	case WALKING:
+		if (walking_count < 14){ // Tengo la secuencia de 15 imagenes y como itero en un array i llega a 14
+			printf("I = %d\n", i);
+			al_draw_bitmap(images_lib[wwalkingseq_arr[walking_count] - 1], 100.0, 100.0, 0);
+			walking_count++;
+		}
+		else {
+			walking_count = 0;
+			state = NOMOTION;
+		}
+
+		break;
+	}
+}
---
+++ PruebaFede/worms_graphic/worms_graphic/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <allegro5\allegro5.h>
+#include <allegro5\allegro_image.h>
+
+
+#include "walk_move.h"
+#include "jump_move.h"
+
+using namespace std;
+
+const float FPS = 50.0;
+
+int main(void) {
+
+	ALLEGRO_DISPLAY * display = nullptr;
+	ALLEGRO_EVENT_QUEUE *	event_queue = nullptr;
+	ALLEGRO_TIMER * timer = nullptr;
+
+
+	if (!al_init()) {
+		return -1;
+	}
+
+	display = al_create_display(800, 600);
+
+	if (display == nullptr) {
+		return -1;
+	}
+
+	
+	timer = al_create_timer(1.0 / FPS);
+	if (!timer) {
+		return -1;
+	}
+
+	if (!al_init_image_addon()) { // ADDON necesario para manejo(no olvidar el freno de mano) de imagenes
+		return -1;
+	}
+
+	event_queue = al_create_event_queue();
+	if (!event_queue) {
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_start_timer(timer);
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+
+	jump_move jmv;
+	jmv.init_jump_vars();
+	jmv.load_jump_imgs();
+
+	walk_move wmv;
+	wmv.init_walk_vars();
+	wmv.load_walk_imgs();
+
+
+	ALLEGRO_EVENT evs;
+
+
+
+	int exit = 0;
+
+	while (!exit) {
+		if (al_get_next_event(event_queue, &evs)) {
+
+			switch (evs.type) {
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				exit = 1;
+				break;
+			case ALLEGRO_EVENT_TIMER:
+
+				//wmv.do_walking_step();
+				jmv.do_jumping_step();
+				//wmv.px--;
+				//wmv.py++;
+				//jmv.px++;
+				//jmv.py++;
+				
+				jmv.orientation = 0;
+				al_flip_display();
+				break;
+			}
+		}
+	}
+	
+	al_destroy_display(display);
+
+	//system("pause");
+	return 0;
+
+}
